aca142906f53bf1fcc03c3d56e3922b2fe79de8e
aarushjos <joshiaarush5@gmail.com>
2025-06-09 15:38:20 +0530

    fixed silly error

--- client/src/components/lobby.jsx
+++ client/src/components/lobby.jsx
@@ -8,7 +8,7 @@ export default function Lobby({ onStart }) {
   const [word, setWord] = useState("");
 
   const handleRandom = () => {
-    const filteredWords = Array.from(guessWords).filter((w) => w.length === 5 && validWords.has(w.toUpperCase()));
+    const filteredWords = Array.from(guessWords).filter((w) => w.length === 5 && validWords.has(w.toLowerCase()));
     const randomWord = filteredWords[Math.floor(Math.random() * filteredWords.length)].toUpperCase();
     setWord(randomWord);
     onStart(null, randomWord, true); // no name needed

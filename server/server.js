3be8b89954e5cac2905a4e6e2369ef6cda246f60
aarushjos <joshiaarush5@gmail.com>
2025-06-09 14:30:57 +0530

    commit 3 server.js issue

--- server/server.js
+++ server/server.js
@@ -1,9 +1,5 @@
 const { WebSocketServer } = require("ws");
-const PORT = process.env.PORT || 8080; // Use Render's port or fallback to 8080
-
-const wss = new WebSocketServer({ port: Number(PORT) });
-
-console.log(`WebSocket server started on port ${PORT}`);
+const wss = new WebSocketServer({ port: 8080 });
 
 wss.on("connection", (ws) => {
   console.log("A player connected");
